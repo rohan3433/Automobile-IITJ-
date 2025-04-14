4b71139a2c15cc53cbb2ab8d9a1a714c64fb7243
Aaditya Kamble <120275055+aadityakamble18@users.noreply.github.com>
2025-04-15 00:17:56 +0530

    Update HeroText.jsx

--- IITJ - Automobile/frontend/src/components/Home/HeroText.jsx
+++ IITJ - Automobile/frontend/src/components/Home/HeroText.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const HeroText = () => {
   return (
    <>
-      <h1 className="text-5xl md:text-6xl text-orange-100 font-bold drop-shadow-lg">
+      <h1 className="text-5xl md:text-6xl text-orange-900 font-bold drop-shadow-lg">
         BOLTHEADS
       </h1>
       <p className="text-xl md:text-2xl text-gray-100 mt-2 drop-shadow-lg italic font-medium animate-fadeIn">
