48972b83fe542903675da2f52fdbb6001ad27720
Aaditya Kamble <120275055+aadityakamble18@users.noreply.github.com>
2025-04-15 00:10:18 +0530

    Update HeroText.jsx

--- IITJ - Automobile/frontend/src/components/Home/HeroText.jsx
+++ IITJ - Automobile/frontend/src/components/Home/HeroText.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const HeroText = () => {
   return (
    <>
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-amber-300 to-white font-extrabold text-5xl md:text-6xl leading-tight drop-shadow-2xl tracking-widest">
+      <h1 className="">
         Automobile Club
       </h1>
       <p className="text-xl md:text-2xl text-gray-100 mt-2 drop-shadow-lg italic font-medium animate-fadeIn">
