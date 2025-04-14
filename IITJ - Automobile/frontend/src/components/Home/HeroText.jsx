87a0cff685558c455b316a3889af3a3d94b32914
Aaditya Kamble <120275055+aadityakamble18@users.noreply.github.com>
2025-04-15 00:13:42 +0530

    Update HeroText.jsx

--- IITJ - Automobile/frontend/src/components/Home/HeroText.jsx
+++ IITJ - Automobile/frontend/src/components/Home/HeroText.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 const HeroText = () => {
   return (
    <>
-      <h1 className="text-5xl md:text-6xl text-gray-100 font-bold drop-shadow-lg">
-        Automobile Club
+      <h1 className="text-5xl md:text-6xl text-orange-100 font-bold drop-shadow-lg">
+        BOULTHEADS
       </h1>
       <p className="text-xl md:text-2xl text-gray-100 mt-2 drop-shadow-lg italic font-medium animate-fadeIn">
         Driven by Passion, United by Wheels
